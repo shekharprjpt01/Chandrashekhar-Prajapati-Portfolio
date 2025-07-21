f84fe569b5b8c35a11145ca5d2eabe7525edc424
Chandrashekhar Prajapati <chandrashekharprajapati92@gmail.com>
2025-07-21 13:52:31 +0530

    Updated resume and portfolio content

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -22,7 +22,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> Chandrashekhar Prjapati</strong>
+                <strong className="main-name"> Chandrashekhar Prajapati</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
